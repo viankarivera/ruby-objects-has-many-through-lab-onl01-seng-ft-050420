d8d57017b90674bbd8c169b0266b3fae004c4443
Vianka Rivera <viankalrivera@gmail.com>
2020-05-20 14:17:15 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -11,16 +11,17 @@ class Doctor
     @@all << self
   end
 
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+  end
+
+
   def new_appointment(patient, date)
     Appointment.new(patient, date, self)
   end
 
   def patients
     appointments.map {|appointment| appointments.patient}
-  end
-
-  def appointments
-    Appointment.all.select {|appointment| appointment.doctor == self}
   end
 
 end
