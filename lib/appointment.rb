f606557ec5d9dae24cee07d8415faabb82e06b59
Vianka Rivera <viankalrivera@gmail.com>
2020-05-20 13:41:39 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,11 @@
+class Appointment 
+  attr_accessor :name, :patient, :doctor
+  @@all = []
+
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self 
+  end 
+end 
